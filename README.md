008ed680c7559070baadcf27921bb7d21f783452
Abhijit Roy <dalvath3700@gmail.com>
2024-03-28 02:26:40 +0530

    Add script for LZ OFT to send tokens faster

--- README.md
+++ README.md
@@ -51,70 +51,22 @@ npx hardhat lz:deploy
 
 > Make sure to follow this [doc](https://docs.layerzero.network/contracts/deploying).
 
-## Run 🚧
+## Run
 
-<details>
-<summary>Tutorial-1: Send message</summary>
+### OApp
 
-> After deployment of the respective OApp contracts on the source and destination chains respectively.
+TODO: write script like [this](./examples/lz-token/src/index.ts).
 
-1. `DesOApp::setPeer` (on Mumbai): Owner set peer in the OApp on the destination chain (say Polygon Mumbai). [Tx url](https://mumbai.polygonscan.com/tx/0xddbe10afe2fbeee4cd70efdbdc26a5f92d1d44827c1a65832aa5557b39e3c615).
+### OFT
 
-  ```
-  eid : 40161
-  peer : 0xc81dcb9afa23cb8483f31b0252a00c93cfc5ac9e000000000000000000000000
-  ```
+Go to [examples/lz-token](./examples/lz-token/)
 
-  > peer is the contract address of `MyOApp` (on the chain-1).
+```sh
+# Install the packages
+yarn
 
-2. Now, need to get fee estimate/quote by the calling `MyOApp::quote` (on Sepolia) before sending message/tx via `MyOApp::send`.
-
-  Input:
-
-  ```
-  _dstEid : 40109
-  _message : "Abhijit is a good boy"
-  _options : 0x00030100110100000000000000000000000000030d40
-  _payInLzToken : false
-  ```
-
-  > `0x00030100110100000000000000000000000000030d40` taken from [Estimating fees](https://docs.layerzero.network/contracts/getting-started#estimating-fees) section.
-
-  2 attempts made because of the requirement of 2 trials:
-
-  ```
-  (nativeFee, lzTokenFee)
-  (uint256,uint256): fee 
-
-  64824974813856,0
-  64881403746362,0
-  ```
-
-3. `MyOApp::send` (on Sepolia): Send message to Mumbai testnet with native fee.
-  Attempts:
-   a. [Tx url](https://sepolia.etherscan.io/tx/0xbd40578f79efda941d381fa33e70261b960af1b8c9e5a9b673e44a5a7a82c7be)
-   b. [Tx url](https://sepolia.etherscan.io/tx/0x408b6b21e0138559f4a25cb961adf5894e4c892ec69a3da023a4b94a0876374e)
-
-  Input:
-
-  msg.value (in wei): 64824974813856
-
-  ```
-
-  _dstEid : 40109
-  _message : "Abhijit is a good boy"
-  _options : 0x00030100110100000000000000000000000000030d40
-
-  ```
-
-  Here, the gas fees is paid to:
-
-- source,
-- destination,
-- Security Stack & Executor, who authenticate and deliver the messages.
-
-4. Check the `data` field of `DestOApp` on Mumbai. If not delivered, track on [LayerZero testnet](https://testnet.layerzeroscan.com/) by tx hash.
-
-> Sometimes, may be due to destination network delay, the message might take long to get delivered.
+# Run the script to send tokens TSSC tokens from chain A to B.
+yarn start
+```
 
 </details>
--- examples/lz-token/src/index.ts
+++ examples/lz-token/src/index.ts
@@ -2,167 +2,270 @@
  * 1 TSSC Token successfully transferred from Sepolia to Mumbai.
  * Tx url: https://testnet.layerzeroscan.com/tx/0x4add361c06fdda00faa7f329ff34d5e1ebc2544104a7cc444f955184dbb74216
  *
- * Takes approx. 5-6 mins.
+ * Takes approx. 5-6 mins to deliver the message to dst chain.
  */
 
-// TODO: Make it as OOP
-
-import { Contract, ethers, BigNumber } from 'ethers'
+import { Contract, ethers, BigNumber, ContractFactory, BigNumberish } from 'ethers'
 import { SendParamStruct, MessagingFeeStruct } from './typechain/contracts/MyToken'
 import { Options } from '@layerzerolabs/lz-v2-utilities'
-
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
-
-// load env var
+import { hexZeroPad } from 'ethers/lib/utils'
 import { config } from 'dotenv'
+
+// load env vars
 config()
 
-import { hexZeroPad } from 'ethers/lib/utils'
+/* Constants */
+// TODO: define max. chains
+// const N: number = 2;
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
-const MyTokenJson = require('../../../artifacts/contracts/MyToken.sol/MyToken.json')
-const abi = MyTokenJson.abi
-
-const epA = process.env.SEPOLIA_ENDPOINT_V2_ID || ''
-const epB = process.env.MUMBAI_ENDPOINT_V2_ID || ''
-
-const tokenOnSepoliaAddress = process.env.MY_LZ_TOKEN_SEPOLIA || ZERO_ADDRESS
-const tokenOnMumbaiAddress = process.env.MY_LZ_TOKEN_MUMBAI || ZERO_ADDRESS
-
-const privateKey: string = process.env.PRIVATE_KEY || ''
-
-/* 
-    The setEnforcedOptions function allows the contract owner to specify mandatory execution options,
-    making sure that the application behaves as expected when users interact with it.
-    Docs: https://docs.layerzero.network/contracts/oft#setting-enforced-options
-
-    TODO: Explore more for its benefits.
-*/
-async function setEnforcedParams(provider: ethers.providers.JsonRpcProvider, contract: Contract) {
-    // Generate the EnforcedOptionParam[] array
-    let enforcedOptions = [
-        {
-            eid: epB,
-            msgType: 1,
-            options: '0x00030100110100000000000000000000000000030d40',
-        },
-        {
-            eid: epB,
-            msgType: 2,
-            options: '',
-        },
-        // ... add more destinations parameters as needed
-    ]
-
-    // Call the setEnforcedOptions function
-    const tx = await contract.setEnforcedOptions(enforcedOptions)
-    // Wait for the transaction to be mined
-    await tx.wait()
-
-    console.log('Enforced options set successfully! with tx hash: ', tx.hash)
+// params loaded from some file(s) like `.env`
+interface BridgeConfig {
+    networkNames: string[]
+    chainRpcUrls: string[]
+    endpointIds: string[]
+    endpointAddresses: string[]
+    tokenAddresses: string[]
+    privateKey: string
+    abi: any
+    bytecode: string
 }
 
-async function checkPeersSend(tokenAddresses: string[], contracts: Contract[], owners: ethers.Wallet[]) {
-    const [tokenOnSepoliaAddress, tokenOnMumbaiAddress] = tokenAddresses
-    const [tokenSepolia, tokenMumbai] = contracts
-    const [ownerSepolia, ownerMumbai] = owners
+class TokenBridge {
+    private providers: ethers.providers.JsonRpcProvider[]
+    public tokens: Contract[]
+    public signers: ethers.Wallet[]
+    public endpointAddresses: string[]
+    public endpointIds: string[]
+    private networkNames: string[]
+    private abi: any
+    private bytecode: string
+    private name: string
+    private symbol: string
 
-    await checkAndSetPeer(tokenSepolia, epB, tokenOnMumbaiAddress, ownerSepolia, 'Sepolia')
-    await checkAndSetPeer(tokenMumbai, epA, tokenOnSepoliaAddress, ownerMumbai, 'Mumbai')
-}
+    constructor(config: BridgeConfig, tokenName: string, tokenSymbol: string) {
+        this.providers = []
+        this.tokens = []
+        this.signers = []
+        this.endpointAddresses = config.endpointAddresses
+        this.endpointIds = config.endpointIds
+        this.networkNames = config.networkNames
+        this.abi = config.abi
+        this.bytecode = config.bytecode
+        this.name = tokenName
+        this.symbol = tokenSymbol
 
-async function checkAndSetPeer(
-    tokenContract: Contract,
-    endpointId: string,
-    peerAddress: string,
-    owner: ethers.Wallet,
-    networkName: string
-) {
-    try {
-        const paddedPeerAddress = hexZeroPad(peerAddress, 32)
-        const isPeerSet = await tokenContract.isPeer(endpointId, paddedPeerAddress)
-        if (!isPeerSet) {
-            console.log(`Incorrect peer was set on ${networkName}`)
-            const tx = await tokenContract.connect(owner).setPeer(endpointId, paddedPeerAddress)
-            await tx.wait()
-            console.log(`Now, correct peer set on ${networkName} via ${tx.hash}`)
+        for (let i = 0; i < 2; i++) {
+            const provider = new ethers.providers.JsonRpcProvider(config.chainRpcUrls[i])
+            this.providers.push(provider)
+            this.signers.push(new ethers.Wallet(config.privateKey, this.providers[i]))
+            this.tokens.push(new ethers.Contract(config.tokenAddresses[i], config.abi, provider))
         }
-    } catch (error) {
-        throw new Error(`Error setting peer on ${networkName} for address ${peerAddress}: ${error}`)
     }
-}
 
-async function sepolia_to_mumbai() {
-    try {
-        // ==== On Sepolia network
-        const sepoliaProvider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL)
-        const tokenSepolia = new ethers.Contract(tokenOnSepoliaAddress, abi, sepoliaProvider)
-        const ownerSepolia = new ethers.Wallet(privateKey, sepoliaProvider)
+    /// NOTE: same abi & bytecode deployed on either chains
+    public async deployTokens(ownerAddresses?: string[]): Promise<void> {
+        const _ownerAddresses = ownerAddresses || this.signers.map((signer) => signer.address)
 
-        // ==== On Mumbai network
-        const mumbaiProvider = new ethers.providers.JsonRpcProvider(process.env.MUMBAI_RPC_URL)
-        const tokenMumbai = new ethers.Contract(tokenOnMumbaiAddress, abi, mumbaiProvider)
-        const ownerMumbai = new ethers.Wallet(privateKey, mumbaiProvider)
+        for (let i = 0; i < 2; i++) {
+            if (this.tokens[i].address === ZERO_ADDRESS) {
+                const tokenFactory: ContractFactory = new ethers.ContractFactory(
+                    this.abi,
+                    this.bytecode,
+                    this.signers[i]
+                )
+                const tokenContract: Contract = await tokenFactory.deploy(
+                    this.name,
+                    this.symbol,
+                    this.endpointAddresses[i],
+                    _ownerAddresses[i]
+                )
+                await tokenContract.deployed()
+                console.log(
+                    `Token-${i} deployed at address: ${tokenContract.address} with tx hash: ${tokenContract.deployTransaction.hash}`
+                )
+                this.tokens.push(tokenContract)
+            }
+        }
+    }
 
-        // check peers, if incorrect set them
-        await checkPeersSend(
-            [tokenOnSepoliaAddress, tokenOnMumbaiAddress],
-            [tokenSepolia, tokenMumbai],
-            [ownerSepolia, ownerMumbai]
+    private static async isPeerSet(
+        tokenContract: Contract,
+        othersEndpointId: string,
+        othersPeerAddress: string
+    ): Promise<boolean> {
+        const paddedPeerAddress = hexZeroPad(othersPeerAddress, 32)
+        return await tokenContract.isPeer(othersEndpointId, paddedPeerAddress)
+    }
+
+    /// most likely it's a utility for `checkPeersSend`
+    private static async checkAndSetPeer(
+        tokenContract: Contract,
+        othersEndpointId: string,
+        othersPeerAddress: string,
+        owner: ethers.Wallet,
+        networkName: string
+    ) {
+        const paddedPeerAddress = hexZeroPad(othersPeerAddress, 32)
+
+        if (!(await TokenBridge.isPeerSet(tokenContract, othersEndpointId, othersPeerAddress))) {
+            console.log(`Incorrect peer was set on ${networkName}`)
+            const tx = await tokenContract.connect(owner).setPeer(othersEndpointId, paddedPeerAddress)
+            await tx.wait()
+            console.log(`Now, correct peer set on ${networkName} via tx hash: ${tx.hash}`)
+        }
+    }
+
+    /// check peers and set (via a tx), if incorrect.
+    public async setPeers(): Promise<void> {
+        await TokenBridge.checkAndSetPeer(
+            this.tokens[0],
+            this.endpointIds[1],
+            this.tokens[1].address,
+            this.signers[0],
+            this.networkNames[0]
         )
+        await TokenBridge.checkAndSetPeer(
+            this.tokens[1],
+            this.endpointIds[0],
+            this.tokens[0].address,
+            this.signers[1],
+            this.networkNames[1]
+        )
+    }
 
-        console.log('Before sending tokens from Sepolia')
-        // get balance of sender/owner
-        // minted 1M tokens to owner during deployment
-        const balOwner = await tokenSepolia.balanceOf(ownerSepolia.address)
-        console.log("Owner's token balance: ", ethers.utils.formatEther(BigInt(balOwner._hex).toString()))
+    public async getBalancesOf(whoAddress: string): Promise<BigNumber[]> {
+        const balances = []
+        for (let i = 0; i < 2; ++i) {
+            const balOwner = await this.tokens[i].balanceOf(whoAddress)
+            console.log(`Owner[${i}]'s token balance: ${ethers.utils.formatEther(balOwner)}`)
+            balances.push(balOwner)
+        }
 
-        const totSupplySepolia = await tokenSepolia.totalSupply()
-        console.log('Token total supply: ', ethers.utils.formatEther(BigInt(totSupplySepolia._hex).toString()))
+        return balances
+    }
 
-        // OPTIONAL: Set enforced params
-        // await setEnforcedParams(provider, token)
+    public async getTotalSuppliesOf(): Promise<BigNumber[]> {
+        const totalSupplies = []
+        for (let i = 0; i < 2; ++i) {
+            const totSupply = await this.tokens[i].totalSupply()
+            console.log(`token[${i}]'s total supply: ${ethers.utils.formatEther(totSupply)}`)
+            totalSupplies.push(totSupply)
+        }
 
+        return totalSupplies
+    }
+
+    /// set enforced params for any contract corresponding to message type like
+    ///     SEND, SEND_CALL, etc. which allows to make different message patterns.
+    public async setEnforcedParams(token: Contract, otherEndpointId: string) {
+        // Generate the EnforcedOptionParam[] array
+        let enforcedOptions = [
+            {
+                eid: otherEndpointId,
+                msgType: 1,
+                options: '0x00030100110100000000000000000000000000030d40', // hex of 200,000 gas
+            },
+            {
+                eid: otherEndpointId,
+                msgType: 2, // SEND_AND_CALL message type
+                options: '',
+            },
+            // ... add more destinations parameters as needed
+        ]
+
+        // Call the setEnforcedOptions function
+        const tx = await token.setEnforcedOptions(enforcedOptions)
+        // Wait for the transaction to be mined
+        await tx.wait()
+
+        console.log('Enforced options set successfully via tx hash: ', tx.hash)
+    }
+
+    /// send tokens from token A to B on 2 different chains
+    public static async sendTokens(
+        srcToken: Contract,
+        srcSigner: ethers.Wallet,
+        amount: BigNumber,
+        dstEid: BigNumberish,
+        dstTokenAddress: string
+    ): Promise<void> {
+        // TODO: add gas limit as param
         const options = Options.newOptions().addExecutorLzReceiveOption(200000, 0).toHex().toString()
         const sendParams: SendParamStruct = {
-            dstEid: epB,
-            to: ethers.utils.hexZeroPad(tokenOnMumbaiAddress, 32),
-            amountLD: BigNumber.from(String(1e18)), // 1 ether
-            minAmountLD: BigNumber.from(String(1e18)), // 1 ether
+            dstEid,
+            to: ethers.utils.hexZeroPad(dstTokenAddress, 32),
+            amountLD: amount,
+            minAmountLD: amount, // TODO: add min amount
             extraOptions: options,
             composeMsg: '0x',
             oftCmd: '0x',
         }
 
         // get quote before send
-        const messagingFee: MessagingFeeStruct = await tokenSepolia.quoteSend(sendParams, false)
+        const messagingFee: MessagingFeeStruct = await srcToken.quoteSend(sendParams, false)
         // console.log('quote: \n nativeFee: ', messagingFee.nativeFee, '\n lzFee: ', messagingFee.lzTokenFee)
 
         // send
-        const sendTx = await tokenSepolia
-            .connect(ownerSepolia)
-            .send(sendParams, messagingFee, ownerSepolia.address, { value: messagingFee.nativeFee })
+        const sendTx = await srcToken
+            .connect(srcSigner)
+            .send(sendParams, messagingFee, srcSigner.address, { value: messagingFee.nativeFee })
         await sendTx.wait()
-
-        console.log('Tx hash for sending tokens on Sepolia: ', sendTx.hash)
-
-        // ==== On Mumbai network
-        console.log('After tokens sent, but before receiving tokens on Mumbai')
-        // get balance of sender/owner
-        const balMumbai = await tokenMumbai.balanceOf(ownerMumbai.address)
-        console.log("Owner's token balance: ", ethers.utils.formatEther(BigInt(balMumbai._hex).toString()))
-        const totSupplyMumbai = await tokenMumbai.totalSupply()
-        console.log('Token total supply: ', ethers.utils.formatEther(BigInt(totSupplyMumbai._hex).toString()))
-    } catch (error) {
-        console.error(`An error occurred in sepolia_to_mumbai: ${error}`)
+        console.log(`Tx hash for sending tokens from chain A: ${sendTx.hash}`)
     }
 }
 
 async function main() {
-    await sepolia_to_mumbai()
+    try {
+        // fetch ABI, Bytecode
+        const MyTokenJson = require('../../../artifacts/contracts/MyToken.sol/MyToken.json')
+
+        // set params mostly from local files like env, etc.
+        const bridgeConfig: BridgeConfig = {
+            networkNames: ['Sepolia', 'Mumbai'],
+            chainRpcUrls: [process.env.SEPOLIA_RPC_URL || '', process.env.MUMBAI_RPC_URL || ''],
+            endpointIds: [process.env.SEPOLIA_ENDPOINT_V2_ID || '', process.env.MUMBAI_ENDPOINT_V2_ID || ''],
+            endpointAddresses: [
+                process.env.SEPOLIA_ENDPOINT_V2 || ZERO_ADDRESS,
+                process.env.MUMBAI_ENDPOINT_V2 || ZERO_ADDRESS,
+            ],
+            tokenAddresses: [
+                process.env.MY_LZ_TOKEN_SEPOLIA || ZERO_ADDRESS,
+                process.env.MY_LZ_TOKEN_MUMBAI || ZERO_ADDRESS,
+            ],
+            privateKey: process.env.PRIVATE_KEY || '',
+            abi: MyTokenJson.abi,
+            bytecode: MyTokenJson.bytecode,
+        }
+
+        // create an instance of token bridge for Sepolia to Mumbai testnets.
+        const tokenBridge = new TokenBridge(bridgeConfig, 'Subspace Token', 'TSSC')
+
+        // Deploy tokens on either chains, if not deployed (depends on .env)
+        // TODO: if freshly deployed, then need to set the address on .env so as to
+        //      take the value from there in next run.
+        await tokenBridge.deployTokens()
+
+        // Set peers, if not set
+        await tokenBridge.setPeers()
+
+        // send tokens from Sepolia to Mumbai
+        // await TokenBridge.sendTokens(
+        //     tokenBridge.tokens[0],
+        //     tokenBridge.signers[0],
+        //     BigNumber.from(String(1e18)), // 1 TSSC
+        //     tokenBridge.endpointIds[1],
+        //     tokenBridge.tokens[1].address
+        // )
+    } catch (error) {
+        // console.error(`${error}`)
+        throw new Error(`Panic: ${error}`)
+    }
 }
 
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error)
+        console.error(`${error}`)
+        process.exit(1)
     })
